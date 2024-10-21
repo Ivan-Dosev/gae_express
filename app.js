16af8620088e152b41d083b2fd9b830e7516ead9
ivan.dimitrov <ivan.dimitrov@fit.fraunhofer.de>
2024-10-21 12:04:52 +0200

    new cors

--- app.js
+++ app.js
@@ -15,11 +15,19 @@ const limiter = rateLimit({
 });
 
 app.use(cors({
-     origin: ['https://play.thedrop.top', 'https://www.play.thedrop.top', 'https://thedrop.top', 'http://localhost:3000'],
-     methods: 'GET,POST', // Specify allowed methods
-     allowedHeaders: 'Content-Type,Authorization', // Specify allowed headers
-     optionsSuccessStatus: 204 // For legacy browsers that choke on 204 responses
- }));
+    origin: ['https://play.thedrop.top', 'https://www.play.thedrop.top', 'https://thedrop.top', 'https://server.thedrop.top', 'http://localhost:3000'],
+    methods: 'GET,POST', // Specify allowed methods
+    allowedHeaders: 'Content-Type,Authorization', // Specify allowed headers
+    optionsSuccessStatus: 204 // For legacy browsers that choke on 204 responses
+}));
+
+// Middleware to log and set headers
+app.use((req, res, next) => {
+   res.header('Access-Control-Allow-Origin', '*'); // You can replace '*' with specific domains if needed
+   res.header('Access-Control-Allow-Methods', 'GET,POST'); // Allow only GET and POST requests
+   res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization'); // Allow Content-Type and Authorization headers
+   next();
+});
 
 // Middleware
 app.use(bodyParser.json());
